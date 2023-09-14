870d10c049d7bb8dc6032e08090f6fec1092d523
Ivar <ivarrobot89@gmail.com>
2023-09-14 19:06:25 +0200

    Primer guardado de este documento

---
+++ SUMA_CON_FOR/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+
+/*
+class Program
+{
+    static void Main()
+    {
+        
+        //Imprimir números del 1 al 10
+
+        for (int i = 1; i<=10; i++) 
+        {
+            Console.WriteLine(i);
+        
+        }
+
+    }
+}
+*/
+
+
+class Program
+{
+    static void Main()
+    {
+        int suma=0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            suma += i;
+
+            Console.WriteLine($"La suma de los numeros es : {suma}");
+        }
+    }
+}
